114cf4e51dc18ea7b8ecdd52b74e7344adf0ec6b
2015kimdh <43131724+2015kimdh@users.noreply.github.com>
2019-05-28 04:56:01 +0900

    Add files via upload

---
+++ pypy.py
@@ -0,0 +1,163 @@
+from tkinter import *
+from tkinter import font
+import tkinter.messagebox
+g_Tk = Tk()
+w = 400
+h = 600
+g_Tk.geometry(str(w)+"x"+str(h)+"+750+200")
+DataList = []
+def InitTopText():
+    TempFont = font.Font(g_Tk, size=12, family = 'Consolas')
+    MainText = Label(g_Tk, font = TempFont, text="경기도 위치 패스트푸드점 검색기능을 제공")
+    MainText.pack()
+    MainText.place(x=w*0.07, y = h*0.6)
+    SigunText = Label(g_Tk, font = TempFont, text="시ㆍ군")
+    SigunText.pack()
+    SigunText.place(x=w * 0.03, y=h / 10 * 7.2)
+    DongText = Label(g_Tk, font=TempFont, text="동 단위")
+    DongText.pack()
+    DongText.place(x=w * 0.03, y=h / 10 * 8.2)
+
+#def InitSearchListBox():
+#
+#    global SearchListBox
+#    ListBoxScrollbar = Scrollbar(g_Tk)
+#    ListBoxScrollbar.pack()
+#    ListBoxScrollbar.place(x=150, y=50)
+#    TempFont = font.Font(g_Tk, size=15, weight='bold',family='Consolas')
+#    SearchListBox = Listbox(g_Tk, font=TempFont,
+#                            activestyle='none',
+#                            width = 10, height = 1, borderwidth = 12, relief = 'ridge',
+#                            yscrollcommand = ListBoxScrollbar.set)
+#    SearchListBox.insert(1, "도서관")
+#    SearchListBox.insert(2, "모범음식점")
+#    SearchListBox.insert(3,"마트")
+#    SearchListBox.insert(4, "문화시설")
+#    SearchListBox.pack()
+#    SearchListBox.place(x=10, y=50)
+#    ListBoxScrollbar.config(command=SearchListBox.yview)
+
+def InitInputLabel():
+    global InputLabel
+    TempFont = font.Font(g_Tk, size=15, weight='bold', family ='Consolas')
+    InputLabel = Entry(g_Tk, font =TempFont, width =26, borderwidth =12, relief ='ridge')
+    InputLabel.pack()
+    InputLabel.place(x=w*0.2, y=h/10*8)
+
+    # InputLabel[1] = Entry(g_Tk, font=TempFont, width=26, borderwidth=12, relief='ridge')
+    # InputLabel[1].pack()
+    # InputLabel[1].place(x=w * 0.2, y=h / 10 * 7)
+
+def InitSearchButton():
+
+    TempFont = font.Font(g_Tk, size=15, weight='bold', family ='Consolas')
+    SearchButton = Button(g_Tk, font =TempFont, text="확 인", command=SearchButtonAction)
+    SearchButton.pack()
+    SearchButton.place(x=w*0.8, y=h/10*9)
+
+def SearchButtonAction():
+
+    #global SearchListBox
+    RenderText.configure(state='normal')
+    RenderText.delete(0.0, END)
+    iSearchIndex =0#SearchListBox.curselection()[0]
+
+    if iSearchIndex == 0:
+        SearchLibrary()
+    elif iSearchIndex == 1:
+            pass#   SearchGoodFoodService()
+    elif iSearchIndex == 2:
+        pass#SearchMarket()
+    elif iSearchIndex == 3:
+        pass#SearchCultural()
+
+    RenderText.configure(state='disabled')
+
+def SearchLibrary():
+        import http.client
+        import urllib
+        from xml.dom.minidom import parse, parseString
+        conn = http.client.HTTPSConnection("openapi.gg.go.kr")
+        hangul_utf8 = urllib.parse.quote(str(InputLabel.get()))
+        conn.request("GET","/Resrestrtfastfood?KEY=fead735fe2264921943cc45687420e65&SIGUN_NM="+hangul_utf8)
+        req = conn.getresponse()
+
+        global DataList
+        DataList.clear()
+
+        if req.status == 200:
+            BooksDoc = req.read().decode('utf-8')
+        if BooksDoc == None:
+            print("에러")
+        else:
+            parseData = parseString(BooksDoc)
+            GeoInfoLibrary = parseData.childNodes
+            row = GeoInfoLibrary[0].childNodes
+
+            print(BooksDoc)
+            for item in row:
+                if item.nodeName == "row":
+                    subitems = item.childNodes
+
+                    if subitems[1].firstChild.nodeValue == InputLabel.get():
+                        pass
+                    # elif subitems[5].firstChild.nodeValue == InputLabel[0].get():
+                    #     pass
+                    else:
+                        continue
+                    if subitems[1].firstChild is not None:
+                        # tel = str(subitems[29].firstChild.nodeValue)
+                        # print(tel)
+                        # pass
+                        # if tel[0] is not '0':
+                        #     tel = "02-" + tel
+                        #     pass
+                        if str(subitems[9].firstChild.nodeValue) == "운영중":
+                            DataList.append((subitems[5].firstChild.nodeValue, subitems[33].firstChild.nodeValue, \
+                                             subitems[39].firstChild.nodeValue, subitems[43].firstChild.nodeValue, \
+                                             subitems[45].firstChild.nodeValue))
+                    else:
+                        if str(subitems[9].firstChild.nodeValue) == "운영중":
+                            DataList.append((subitems[5].firstChild.nodeValue, subitems[33].firstChild.nodeValue, \
+                                             subitems[39].firstChild.nodeValue, subitems[43].firstChild.nodeValue,\
+                                             subitems[45].firstChild.nodeValue))
+
+            for i in range(len(DataList)):
+                RenderText.insert(INSERT, "[")
+                RenderText.insert(INSERT, i + 1)
+                RenderText.insert(INSERT, "] ")
+                RenderText.insert(INSERT, "시설명: ")
+                RenderText.insert(INSERT, DataList[i][0])
+                RenderText.insert(INSERT, "\n")
+                RenderText.insert(INSERT, "종류: ")
+                RenderText.insert(INSERT, DataList[i][1])
+                RenderText.insert(INSERT, "\n")
+                RenderText.insert(INSERT, "도로명주소: ")
+                RenderText.insert(INSERT, DataList[i][1])
+                RenderText.insert(INSERT, "\n\n")
+
+def InitRenderText():
+   global RenderText
+   RenderTextScrollbar = Scrollbar(g_Tk)
+   RenderTextScrollbar.place(x=w*0.9, y=h/30, height = h/10*5.2)
+   RenderTextScrollbar#.place(x=375, y=200)
+
+   TempFont = font.Font(g_Tk, size=10, family='Consolas')
+   RenderText = Text(g_Tk, width=45, height=22,borderwidth=12, relief='ridge', yscrollcommand=RenderTextScrollbar.set)
+   RenderText.pack()
+   RenderText.place(x=w/20, y=h/30)
+   RenderTextScrollbar.config(command=RenderText.yview)
+   # RenderTextScrollbar.pack(side=RIGHT, fill=Y)
+
+   RenderText.configure(state='disabled')
+
+    #…
+InitTopText()
+#InitSearchListBox()
+InitInputLabel()
+InitSearchButton()
+InitRenderText()
+#InitSendEmailButton()
+#InitSortListBox()
+#InitSortButton()
+g_Tk.mainloop()
