ab56e2b810971b0a0da3fe0080d05d0b8d62b761
2015kimdh <43131724+2015kimdh@users.noreply.github.com>
2019-05-29 02:33:26 +0900

    지식의 정수
    지식의 정수

--- pypy.py
+++ pypy.py
@@ -6,7 +6,13 @@ w = 400
 h = 600
 g_Tk.geometry(str(w)+"x"+str(h)+"+750+200")
 DataList = []
+FrameNum = 0
+
 def InitTopText():
+    global MainText
+    global SigunText
+    global DongText
+
     TempFont = font.Font(g_Tk, size=12, family = 'Consolas')
     MainText = Label(g_Tk, font = TempFont, text="경기도 위치 패스트푸드점 검색기능을 제공")
     MainText.pack()
@@ -60,11 +66,13 @@ def InitSearchButton():
     SearchButton.pack()
     SearchButton.place(x=w*0.8, y=h/10*9)
 
+    Search1Button = Button(g_Tk, font=TempFont, text="갱 신", command=WizetClear)
+    Search1Button.pack()
+    Search1Button.place(x=w * 0.2, y=h / 10 * 9)
+
 def SearchButtonAction():
 
     #global SearchListBox
-    RenderText.configure(state='normal')
-    RenderText.delete(0.0, END)
     iSearchIndex =0#SearchListBox.curselection()[0]
 
     if iSearchIndex == 0:
@@ -76,7 +84,6 @@ def SearchButtonAction():
     elif iSearchIndex == 3:
         pass#SearchCultural()
 
-    RenderText.configure(state='disabled')
 
 
 def SearchLibrary():
@@ -99,7 +106,6 @@ def SearchLibrary():
     else:
         from xml.etree import ElementTree
         tree = ElementTree.fromstring(BooksDoc)
-        print(BooksDoc)
 
         itemElements = tree.getiterator("row")
 
@@ -115,25 +121,6 @@ def SearchLibrary():
                 #     return {"NM":name.text, "addr":addr.text}\
                 if status.text == "운영중":
                     DataList.append((name.text, addr.text, addr2.text, wido.text, gyungdo.text))
-                    RenderText.insert(INSERT, "[")
-                    RenderText.insert(INSERT, i + 1)
-                    RenderText.insert(INSERT, "] ")
-                    RenderText.insert(INSERT, "시설명: ")
-                    if name.text != None:
-                        # RenderText.insert(INSERT, name.text)
-                        RenderText.insert(INSERT, DataList[i][0])
-                    RenderText.insert(INSERT, "\n")
-
-                    if addr.text != None:
-                        RenderText.insert(INSERT, "도로명주소: ")
-                        # RenderText.insert(INSERT, addr.text)
-                        RenderText.insert(INSERT, DataList[i][1])
-                    else:
-                        RenderText.insert(INSERT, "지번주소: ")
-                        # RenderText.insert(INSERT, addr2.text)
-                        RenderText.insert(INSERT, DataList[i][2])
-                    RenderText.insert(INSERT, "\n\n")
-                    i += 1
         else:
             for row in itemElements:
                 name = row.find("BIZPLC_NM")
@@ -148,59 +135,200 @@ def SearchLibrary():
                     if addr.text != None:
                         if InputLabel1.get() in addr.text:
                             DataList.append((name.text, addr.text, addr2.text, wido.text, gyungdo.text))
-                            RenderText.insert(INSERT, "[")
-                            RenderText.insert(INSERT, i + 1)
-                            RenderText.insert(INSERT, "] ")
-                            RenderText.insert(INSERT, "시설명: ")
-                            if name.text != None:
-                                # RenderText.insert(INSERT, name.text)
-                                RenderText.insert(INSERT, DataList[i][0])
-                            RenderText.insert(INSERT, "\n")
-                            RenderText.insert(INSERT, "도로명주소: ")
-                            # RenderText.insert(INSERT, addr.text)
-                            RenderText.insert(INSERT, DataList[i][1])
-                            RenderText.insert(INSERT, "\n\n")
-                            i += 1
-                    else:
-                        if InputLabel1.get() in addr2.text:
-                            DataList.append((name.text, addr.text, addr2.text, wido.text, gyungdo.text))
-                            RenderText.insert(INSERT, "[")
-                            RenderText.insert(INSERT, i + 1)
-                            RenderText.insert(INSERT, "] ")
-                            RenderText.insert(INSERT, "시설명: ")
-                            if name.text != None:
-                                # RenderText.insert(INSERT, name.text)
-                                RenderText.insert(INSERT, DataList[i][0])
-                            RenderText.insert(INSERT, "\n")
-                            RenderText.insert(INSERT, "지번주소: ")
-                            # RenderText.insert(INSERT, addr2.text)
-                            RenderText.insert(INSERT, DataList[i][2])
-                            RenderText.insert(INSERT, "\n\n")
-                            i += 1
+
+#def SearchLibrary():
+#    import http.client
+#    import urllib
+#    from xml.dom.minidom import parse, parseString
+#    conn = http.client.HTTPSConnection("openapi.gg.go.kr")
+#    hangul_utf8 = urllib.parse.quote(str(InputLabel.get()))
+#    conn.request("GET", "/Resrestrtfastfood?KEY=fead735fe2264921943cc45687420e65&pSize=1000&SIGUN_NM=" + hangul_utf8)
+#    req = conn.getresponse()
+#    i = 0
+#
+#    global DataList
+#    DataList.clear()
+#
+#    if req.status == 200:
+#        BooksDoc = req.read().decode('utf-8')
+#    if BooksDoc == None:
+#        print("에러")
+#    else:
+#        from xml.etree import ElementTree
+#        tree = ElementTree.fromstring(BooksDoc)
+#
+#        itemElements = tree.getiterator("row")
+#
+#        if InputLabel1.get() == "":
+#            for row in itemElements:
+#                name = row.find("BIZPLC_NM")
+#                status = row.find("BSN_STATE_NM")
+#                addr = row.find("REFINE_ROADNM_ADDR")
+#                addr2 = row.find("REFINE_LOTNO_ADDR")
+#                wido = row.find("REFINE_WGS84_LAT")
+#                gyungdo = row.find("REFINE_WGS84_LOGT")
+#                # if len(addr.text) > 0:
+#                #     return {"NM":name.text, "addr":addr.text}\
+#                if status.text == "운영중":
+#                    DataList.append((name.text, addr.text, addr2.text, wido.text, gyungdo.text))
+#                    RenderText_1.insert(INSERT, "[")
+#                    RenderText_1.insert(INSERT, i + 1)
+#                    RenderText_1.insert(INSERT, "] ")
+#                    RenderText_1.insert(INSERT, "시설명: ")
+#                    if name.text != None:
+#                        # RenderText.ins#ert(INSERT, name.text)
+#                        RenderText_1.insert(INSERT, DataList[i][0])
+#                    RenderText_1.insert(INSERT, "\n")
+#
+#                    if addr.text != None:
+#                        RenderText_1.insert(INSERT, "도로명주소: ")
+#                        # RenderText.insert(INSERT, addr.text)
+#                        RenderText_1.insert(INSERT, DataList[i][1])
+#                    else:
+#                        RenderText_1.insert(INSERT, "지번주소: ")
+#                        # RenderText.insert(INSERT, addr2.text)
+#                        RenderText_1.insert(INSERT, DataList[i][2])
+#                        RenderText_1.insert(INSERT, "\n\n")
+#                    i += 1
+#        else:
+#            for row in itemElements:
+#                name = row.find("BIZPLC_NM")
+#                status = row.find("BSN_STATE_NM")
+#                addr = row.find("REFINE_ROADNM_ADDR")
+#                addr2 = row.find("REFINE_LOTNO_ADDR")
+#                wido = row.find("REFINE_WGS84_LAT")
+#                gyungdo = row.find("REFINE_WGS84_LOGT")
+#                # if len(addr.text) > 0:
+#                #     return {"NM":name.text, "addr":addr.text}\
+#                if status.text == "운영중":
+#                    if addr.text != None:
+#                        if InputLabel1.get() in addr.text:
+#                            DataList.append((name.text, addr.text, addr2.text, wido.text, gyungdo.text))
+#                            RenderText_1.insert(INSERT, "[")
+#                            RenderText_1.insert(INSERT, i + 1)
+#                            RenderText_1.insert(INSERT, "] ")
+#                            RenderText_1.insert(INSERT, "시설명: ")
+#                            if name.text != None:
+#                                # RenderText.insert(INSERT, name.text)
+#                                RenderText_1.insert(INSERT, DataList[i][0])
+#                            RenderText_1.insert(INSERT, "\n")
+#                            RenderText_1.insert(INSERT, "도로명주소: ")
+#                            # RenderText.insert(INSERT, addr.text)
+#                            RenderText_1.insert(INSERT, DataList[i][1])
+#                            RenderText_1.insert(INSERT, "\n\n")
+#                            i += 1
+#                    else:
+#                        if InputLabel1.get() in addr2.text:
+#                            DataList.append((name.text, addr.text, addr2.text, wido.text, gyungdo.text))
+#                            RenderText_1.insert(INSERT, "[")
+#                            RenderText_1.insert(INSERT, i + 1)
+#                            RenderText_1.insert(INSERT, "] ")
+#                            RenderText_1.insert(INSERT, "시설명: ")
+#                            if name.text != None:
+#                                # RenderText.insert(INSERT, name.text)
+#                                RenderText_1.insert(INSERT, DataList[i][0])
+#                            RenderText_1.insert(INSERT, "\n")
+#                            RenderText_1.insert(INSERT, "지번주소: ")
+#                            # RenderText.insert(INSERT, addr2.text)
+#                            RenderText_1.insert(INSERT, DataList[i][2])
+#                            RenderText_1.insert(INSERT, "\n\n")
+#                            i += 1
 
 
 def InitRenderText():
    global RenderText
    RenderTextScrollbar = Scrollbar(g_Tk)
-   RenderTextScrollbar.place(x=w*0.9, y=h/30, height = h/10*5.2)
+   RenderTextScrollbar.place(x=w*0.9, y=h/10 * 7, height = h/10*1.5)
    RenderTextScrollbar#.place(x=375, y=200)
 
    TempFont = font.Font(g_Tk, size=10, family='Consolas')
-   RenderText = Text(g_Tk, width=45, height=22,borderwidth=12, relief='ridge', yscrollcommand=RenderTextScrollbar.set)
+   RenderText = Text(g_Tk, width=45, height=5,borderwidth=12, relief='ridge', yscrollcommand=RenderTextScrollbar.set)
+   j = 0
+
+   for i in DataList:
+       RenderText.insert(INSERT, "[")
+       RenderText.insert(INSERT, j + 1)
+       RenderText.insert(INSERT, "] ")
+       RenderText.insert(INSERT, "시설명: ")
+       if i[0] != None:
+           # RenderText.ins#ert(INSERT, name.text)
+           RenderText.insert(INSERT, i[0])
+       RenderText.insert(INSERT, "\n")
+
+       if i[1] != None:
+           RenderText.insert(INSERT, "도로명주소: ")
+           # RenderText.insert(INSERT, addr.text)
+           RenderText.insert(INSERT, i[1])
+           RenderText.insert(INSERT, "\n\n")
+       else:
+           RenderText.insert(INSERT, "지번주소: ")
+           # RenderText.insert(INSERT, addr2.text)
+           RenderText.insert(INSERT, i[2])
+           RenderText.insert(INSERT, "\n\n")
+       j += 1
+
+
+
    RenderText.pack()
-   RenderText.place(x=w/20, y=h/30)
+   RenderText.place(x=w/20, y=h/10 * 7)
    RenderTextScrollbar.config(command=RenderText.yview)
    # RenderTextScrollbar.pack(side=RIGHT, fill=Y)
 
+
+
    RenderText.configure(state='disabled')
 
+def InitRenderText_1():
+    global RenderText_1
+    global RenderText_1Scrollbar
+    RenderText_1Scrollbar = Scrollbar(g_Tk)
+    RenderText_1Scrollbar.place(x=w * 0.9, y=h / 30, height=h / 10 * 5.2)
+    RenderText_1Scrollbar  # .place(x=375, y=200)
+
+    TempFont = font.Font(g_Tk, size=10, family='Consolas')
+    RenderText_1 = Text(g_Tk, width=45, height=22, borderwidth=12, relief='ridge',
+                      yscrollcommand=RenderText_1Scrollbar.set)
+    RenderText_1.pack()
+    RenderText_1.place(x=w / 20, y=h / 30)
+    RenderText_1Scrollbar.config(command=RenderText_1.yview)
+    # RenderTextScrollbar.pack(side=RIGHT, fill=Y)
+
+    RenderText_1.configure(state='disabled')
+
     #…
+
+def WizetClear():
+
+    global  FrameNum
+    FrameNum += 1
+    FrameNum = FrameNum % 2
+
+    if FrameNum == 0:
+        RenderText.destroy()
+        InitTopText()
+        # InitSearchListBox()
+        InitInputLabel()
+        InitRenderText_1()
+        InitSearchButton()
+        # InitSendEmailButton()
+        # InitSortListBox()
+        # InitSortButton()
+    elif FrameNum == 1:
+        RenderText_1.destroy()
+        RenderText_1Scrollbar.destroy()
+        InputLabel.destroy()
+        InputLabel1.destroy()
+        SigunText.destroy()
+        MainText.destroy()
+        DongText.destroy()
+        InitRenderText()
+
 InitTopText()
-#InitSearchListBox()
+# InitSearchListBox()
 InitInputLabel()
+InitRenderText_1()
 InitSearchButton()
-InitRenderText()
-#InitSendEmailButton()
-#InitSortListBox()
-#InitSortButton()
+
+g_Tk.update()
+g_Tk.after_cancel(g_Tk)
 g_Tk.mainloop()
