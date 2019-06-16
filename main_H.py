b090f0ee7983169189b709274c78345cd3e3ffec
JungHeeSeung <moss2223@naver.com>
2019-06-17 03:31:47 +0900

    프레임 전환 시도
    이제 내가 작업하는 파일은 _H를 붙일게

--- main_H.py
+++ main_H.py
@@ -1,5 +1,6 @@
 # 내가 테스트로 분리해본 메인임
 # 쓸지 말지는 다음에 만나서 결정하자
+# 무언가를 추가 하고싶으면 해당 프레임 파일로 가서 수정할 것
 
 from tkinter import *
 from tkinter import font
@@ -14,5 +15,12 @@ g_Tk.title("패스트푸드점 검색기")
 DataList = []
 
 mainFrame = mainframe_H.MainFrame(g_Tk, w, h)
+secondFrame = Frame(g_Tk, width=w, height=h)
+a = Label(secondFrame, width=40, height=40, text="두번째 프레임이다!")
+a.pack()
+secondFrame.pack()
+
+
+secondFrame.tkraise()
 
 g_Tk.mainloop()
